b4fe53ed6e1b519514fc355904b57c661d63122a
bilaljanir <bilaljanir@gmail.com>
2023-05-08 10:28:12 +0200

    add table and migration

---
+++ database/migrations/2023_05_08_080115_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_fr');
+            $table->string('title_en');
+            $table->string('slug');
+            $table->text('body_fr');
+            $table->text('body_en');
+            $table->boolean('premium')->default(0);
+            $table->boolean('published');
+            $table->string('photo');
+            $table->foreignId('admin_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
