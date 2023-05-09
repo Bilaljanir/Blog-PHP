d05134966981cf1ed3396795c4279fa8fd710de7
bilaljanir <bilaljanir@gmail.com>
2023-05-09 10:54:34 +0200

    config bootstrap and fortify

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('title')
+Home
+@endsection
+
+@section('content')
+
+    <div class="row my-5">
+        <div class="col md-8">
+    Posts
+        </div>
+        <div class="col md-8">
+        categories
+        </div>
+
+        </div>
+
+@endsection
