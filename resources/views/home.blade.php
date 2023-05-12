6505532c580985ea76a6602923d220685453482f
bilaljanir <bilaljanir@gmail.com>
2023-05-12 16:33:55 +0200

    composition API post detail add time and user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,28 +15,28 @@
         <div class="col-md-8">
             <div class="card p-4 ">
                 <div class="row">
-    @isset($postsPremium)
-    @foreach($postsPremium as $post)
-    <div class="col-md-4 mb-2">
-        <div class="card h-100">
-            <img src="{{$posts->photo}}"
-                 class="card-img-top"
-                 alt="{{$post->title_en}}">
-            <div class="card-body">
-                <div class="card-title">
-                    {{$post_title_en}}
+                    @isset($postsPremium)
+                        @foreach($postsPremium as $post)
+                            <div class="col-md-4 mb-2">
+                                <div class="card h-100">
+                                    <img src="{{$posts->photo}}"
+                                         class="card-img-top"
+                                         alt="{{$post->title_en}}">
+                                    <div class="card-body">
+                                        <div class="card-title fw-bold">
+                                            {{$post_title_en}}
 
-                </div>
-                <p class="card-text">
-                    {{  Str::limit($post->body_en, 100)}}
-                </p>
-                <a href="{{route('posts.show',$post)}}" class="btn btn-primary"></a>
-            </div>
-        </div>
-    </div>
+                                        </div>
+                                        <p class="card-text">
+                                            {{  Str::limit($post->body_en, 100)}}
+                                        </p>
+                                        <a href="{{route('posts.show',$post)}}" class="btn btn-primary"></a>
+                                    </div>
+                                </div>
+                            </div>
 
-    @endforeach
-    @endisset
+                        @endforeach
+                    @endisset
                     @foreach($posts as $post)
                         <div class="col-md-4 mb-2">
                             <div class="card h-100">
@@ -44,7 +44,7 @@
                                      class="card-img-top"
                                      alt="{{$post->title_en}}">
                                 <div class="card-body">
-                                    <div class="card-title">
+                                    <div class="card-title fw-bold">
                                         {{$post->title_en}}
 
                                     </div>
@@ -59,29 +59,30 @@
                         </div>
 
                     @endforeach
-            </div>
+                </div>
                 <div class="card-footer bg-white">
                     <div class="d-flex justify-content-center">
                         {{$posts->links()}}
                     </div>
                 </div>
-        </div>
+            </div>
         </div>
 
         <div class="col-md-4 ">
-                <ul class="list-group">
-                    @foreach($categories as $category)
-                        <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <a href="{{route('category.posts', $category)}}" class="btn btn link text-decoration-none text-dark">
-                                {{$category->name_en}}
+            <ul class="list-group">
+                @foreach($categories as $category)
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <a href="{{route('category.posts', $category)}}"
+                           class="btn btn link text-decoration-none text-dark">
+                            {{$category->name_en}}
                         </a>
-                            <span class="badge bg-primary rounded-pill">
+                        <span class="badge bg-primary rounded-pill">
                             {{$category->posts()->count()}}
                             </span>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
 
     </div>
 @endsection
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -9,25 +9,38 @@
         <div class="col-md-8">
             <div class="card p-4 ">
                 <div class="row">
-                        <div class="col-md-12 mb-2">
-                            <div class="card h-100">
-                                <img src="{{$post->photo}}"
-                                     class="card-img-top"
-                                     alt="{{$post->title_en}}">
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        {{$post->title_en}}
-
-                                    </div>
-                                    <p class="card-text">
-                                        {{ $post->body_en }}
-                                    </p>
+                    <div class="col-md-12 mb-2">
+                        <div class="card h-100">
+                            <img src="{{$post->photo}}"
+                                 class="card-img-top"
+                                 alt="{{$post->title_en}}">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-center my-3">
+                                    <span class="badge bg-danger">
+                                        <i class="fas fa-clock me-1"></i>
+                                        {{$post->created_at->diffForHumans()}}
+                                    </span>
+                                    <span class="badge bg-success mx-2">
+                                        <i class="fas fa-user me-1"></i>
+                                        {{$post->admin->name}}
+                                    </span>
+                                    <span class="badge bg-primary">
+                                        <i class="fas fa-tag me-1"></i>
+                                            {{$post->category->name_en}}
+                                    </span>
                                 </div>
+                                <div class="card-title fw-bold">
+                                    {{$post->title_en}}
+
+                                </div>
+                                <p class="card-text">
+                                    {{ $post->body_en }}
+                                </p>
                             </div>
                         </div>
+                    </div>
                 </div>
 
-                </div>
             </div>
         </div>
 
@@ -35,7 +48,8 @@
             <ul class="list-group">
                 @foreach($categories as $category)
                     <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <a href="{{route('category.posts', $category)}}" class="btn btn link text-decoration-none text-dark">
+                        <a href="{{route('category.posts', $category)}}"
+                           class="btn btn link text-decoration-none text-dark">
                             {{$category->name_en}}
                         </a>
                         <span class="badge bg-primary rounded-pill">
@@ -46,5 +60,4 @@
             </ul>
         </div>
 
-    </div>
 @endsection
