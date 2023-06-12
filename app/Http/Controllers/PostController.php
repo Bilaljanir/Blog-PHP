e0b0e0ba1615347bccc3b1b16fa8d934d0f496f9
bilaljanir <bilaljanir@gmail.com>
2023-06-12 11:20:36 +0200

    page creat posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,7 +21,9 @@ class PostController extends Controller
 
     public function create()
     {
-    }
+        return view('admin.posts.create')->with([
+            'tags' => Tag::all()
+        ]);    }
 
     public function store(StorePostRequest $request)
     {
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,98 +9,66 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                   <div>
+                    <div>
                         <h4 class="card-title">
                             <a href="{{route('posts.create')}}" class="btn btn-sm btn-primary">
                                 <i class="fas fa-plus"></i>
                             </a>
                         </h4>
-                   </div>
-                   <hr>
-                   <table class="table table-hovered">
+                    </div>
+                    <hr>
+                    <table class="table table-hovered">
                         <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Category</th>
-                                <th>By</th>
-                                <th>Premium</th>
-                                <th>Published</th>
-                                <th>Image</th>
-                                <th>Added</th>
-                                <th></th>
-                            </tr>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>By</th>
+                            <th>Image</th>
+                            <th>Added</th>
+                            <th></th>
+                        </tr>
                         </thead>
                         <tbody>
-                            @foreach ($posts as $key => $post)
-                                <tr>
-                                    <td>{{$key+=1}}</td>
-                                    <td>
-                                        <a href="{{route('posts.show',$post)}}" target="_blank">
-                                            {{$post->title_en}}
-                                        </a>
-                                    </td>
-                                    <td>{{$post->category->name_en}}</td>
-                                    <td>{{$post->admin->name}}</td>
-                                    <td>
-                                        @if ($post->premium)
-                                            <a href="{{route('toggle.premium', $post)}}">
-                                                <span class="badge bg-success">
-                                                    premium
-                                                </span>
-                                            </a>
-                                        @else
-                                            <a href="{{route('toggle.premium', $post)}}">
-                                                <span class="badge bg-info">
-                                                    simple
-                                                </span>
-                                            </a>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if ($post->published)
-                                            <a href="{{route('toggle.published', $post)}}">
-                                                <span class="badge bg-success">
-                                                    published
-                                                </span>
-                                            </a>
-                                        @else
-                                            <a href="{{route('toggle.published', $post)}}">
-                                                <span class="badge bg-info">
-                                                    draft
-                                                </span>
-                                            </a>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <img src="{{asset($post->photo)}}"
-                                            width="50"
-                                            height="45"
-                                            class="rounded-1"
-                                            alt="{{$post->title_en}}">
-                                    </td>
-                                    <td>
-                                        {{$post->created_at->diffForHumans()}}
-                                    </td>
-                                    <td class="d-flex">
-                                        <a href="{{route('posts.edit',$post)}}" class="btn btn-sm btn-warning">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                        <button onclick="
+                        @foreach ($posts as $key => $post)
+                            <tr>
+                                <td>{{$key+=1}}</td>
+                                <td>
+                                    <a href="{{route('posts.show',$post)}}" target="_blank">
+                                        {{$post->title_en}}
+                                    </a>
+                                </td>
+                                <td>{{$post->category->name_en}}</td>
+                                <td>{{$post->admin->name}}</td>
+                                <td>
+                                    <img src="{{asset($post->photo)}}"
+                                         width="60"
+                                         height="60"
+                                         class="rounded"
+                                         alt="{{$post->title_en}}">
+                                </td>
+                                <td>
+                                    {{$post->created_at->diffForHumans()}}
+                                </td>
+                                <td class="d-flex">
+                                    <a href="{{route('posts.edit',$post)}}" class="btn btn-sm btn-warning">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <button onclick="
                                                         if(confirm('are you sure ?'))
                                                         document.getElementById({{$post->id}}).submit();
                                                         " class="btn btn-sm btn-danger mx-2">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                        <form id="{{$post->id}}" action="{{route('posts.destroy',$post)}}" method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                    <form id="{{$post->id}}" action="{{route('posts.destroy',$post)}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
                         </tbody>
-                   </table>
+                    </table>
                 </div>
                 <div class="d-flex justify-content-center">
                     {{$posts->links()}}
@@ -108,4 +76,13 @@
             </div>
         </div>
     </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>&copy; 2023 create by bilal janir  </p>
+                </div>
+            </div>
+        </div>
+    </footer>
 @endsection
